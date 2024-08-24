d00e64ce718564d4f59c323293e4881b50fbeb1a
Merve Aydin <110039353+merveaydincode@users.noreply.github.com>
2024-08-24 19:09:16 +0300

    castle/desert facility added.

--- assets/data/castles.ts
+++ assets/data/castles.ts
@@ -8,100 +8,130 @@ const castles: Hotels = [
     name: 'Hogwarts Castle',
     location: 'Scotland, United Kingdom',
     image: require('../CastleImage/c1.png'),
-    rooms: [
-      { roomId: 1, type: 'Single', price: 500, bedCount: 1, bathroomCount: 1, hasBarbecue: false, hasPool: true, hasKitchen: true },
-      { roomId: 2, type: 'Double', price: 800, bedCount: 2, bathroomCount: 2, hasBarbecue: false, hasPool: true, hasKitchen: true }
-    ]
+    coordinate: {
+      latitude: 0,
+      longitude: 0
+    },
+    price: 500,
+    star: '4,5',
+    images: undefined
   },
   {
     id: 2,
     name: 'Neuschwanstein Castle',
     location: 'Bavaria, Germany',
     image: require('../CastleImage/c2.png'),
-    rooms: [
-      { roomId: 1, type: 'Single', price: 450, bedCount: 1, bathroomCount: 1, hasBarbecue: false, hasPool: true, hasKitchen: true },
-      { roomId: 2, type: 'Suite', price: 750, bedCount: 2, bathroomCount: 2, hasBarbecue: false, hasPool: true, hasKitchen: true }
-    ]
+    coordinate: {
+      latitude: 0,
+      longitude: 0
+    },
+    price: 600,
+    star: '4,7',
+    images: undefined
   },
   {
     id: 3,
     name: 'Château de Chambord',
     location: 'Loir-et-Cher, France',
     image: require('../CastleImage/c3.png'),
-    rooms: [
-      { roomId: 1, type: 'Single', price: 400, bedCount: 1, bathroomCount: 1, hasBarbecue: true, hasPool: true, hasKitchen: true },
-      { roomId: 2, type: 'Double', price: 700, bedCount: 2, bathroomCount: 2, hasBarbecue: true, hasPool: true, hasKitchen: true }
-    ]
+    coordinate: {
+      latitude: 0,
+      longitude: 0
+    },
+    price: 700,
+    star: '4,8',
+    images: undefined
   },
   {
     id: 4,
     name: 'Castle of the Moors',
     location: 'Sintra, Portugal',
     image: require('../CastleImage/c4.png'),
-    rooms: [
-      { roomId: 1, type: 'Single', price: 420, bedCount: 1, bathroomCount: 1, hasBarbecue: false, hasPool: false, hasKitchen: true },
-      { roomId: 2, type: 'Double', price: 650, bedCount: 2, bathroomCount: 1, hasBarbecue: false, hasPool: false, hasKitchen: true }
-    ]
+    coordinate: {
+      latitude: 0,
+      longitude: 0
+    },
+    price: 200,
+    star: '4,2',
+    images: undefined
   },
   {
     id: 5,
     name: 'Edinburgh Castle',
     location: 'Edinburgh, Scotland',
     image: require('../CastleImage/c5.png'),
-    rooms: [
-      { roomId: 1, type: 'Single', price: 430, bedCount: 1, bathroomCount: 1, hasBarbecue: false, hasPool: false, hasKitchen: true },
-      { roomId: 2, type: 'Double', price: 660, bedCount: 2, bathroomCount: 1, hasBarbecue: false, hasPool: false, hasKitchen: true }
-    ]
+    coordinate: {
+      latitude: 0,
+      longitude: 0
+    },
+    price: 500,
+    star: '3,1',
+    images: undefined
   },
   {
     id: 6,
     name: 'Hohenzollern Castle',
     location: 'Swabian Alps, Germany',
     image: require('../CastleImage/c6.png'),
-    rooms: [
-      { roomId: 1, type: 'Single', price: 470, bedCount: 1, bathroomCount: 1, hasBarbecue: true, hasPool: false, hasKitchen: true },
-      { roomId: 2, type: 'Suite', price: 720, bedCount: 2, bathroomCount: 2, hasBarbecue: true, hasPool: false, hasKitchen: true }
-    ]
+    coordinate: {
+      latitude: 0,
+      longitude: 0
+    },
+    price: 500,
+    star: '3,4',
+    images: undefined
   },
   {
     id: 7,
     name: 'Castle of Neuschwanstein',
     location: 'Bavaria, Germany',
     image: require('../CastleImage/c7.png'),
-    rooms: [
-      { roomId: 1, type: 'Single', price: 480, bedCount: 1, bathroomCount: 1, hasBarbecue: true, hasPool: true, hasKitchen: true },
-      { roomId: 2, type: 'Double', price: 730, bedCount: 2, bathroomCount: 2, hasBarbecue: true, hasPool: true, hasKitchen: true }
-    ]
+    coordinate: {
+      latitude: 0,
+      longitude: 0
+    },
+    price: 600,
+    star: '3,7',
+    images: undefined
   },
   {
     id: 8,
     name: 'Castle of the White Rose',
     location: 'Würzburg, Germany',
     image: require('../CastleImage/c8.png'),
-    rooms: [
-      { roomId: 1, type: 'Single', price: 440, bedCount: 1, bathroomCount: 1, hasBarbecue: false, hasPool: true, hasKitchen: true },
-      { roomId: 2, type: 'Suite', price: 680, bedCount: 2, bathroomCount: 2, hasBarbecue: false, hasPool: true, hasKitchen: true }
-    ]
+    coordinate: {
+      latitude: 0,
+      longitude: 0
+    },
+    price: 700,
+    star: '3,9',
+    images: undefined
   },
   {
     id: 9,
     name: 'Bran Castle',
     location: 'Bran, Romania',
     image: require('../CastleImage/c9.png'),
-    rooms: [
-      { roomId: 1, type: 'Single', price: 460, bedCount: 1, bathroomCount: 1, hasBarbecue: false, hasPool: false, hasKitchen: true },
-      { roomId: 2, type: 'Double', price: 690, bedCount: 2, bathroomCount: 1, hasBarbecue: false, hasPool: false, hasKitchen: true }
-    ]
+    coordinate: {
+      latitude: 0,
+      longitude: 0
+    },
+    price: 200,
+    star: '4,2',
+    images: undefined
   },
   {
     id: 10,
     name: 'Château de Versailles',
     location: 'Versailles, France',
     image: require('../CastleImage/c10.png'),
-    rooms: [
-      { roomId: 1, type: 'Single', price: 550, bedCount: 1, bathroomCount: 1, hasBarbecue: false, hasPool: true, hasKitchen: true },
-      { roomId: 2, type: 'Suite', price: 850, bedCount: 2, bathroomCount: 2, hasBarbecue: false, hasPool: true, hasKitchen: true }
-    ]
+    coordinate: {
+      latitude: 0,
+      longitude: 0
+    },
+    price: 500,
+    star: '3,1',
+    images: undefined
   }
 ];
 
--- assets/data/desertHotels.ts
+++ assets/data/desertHotels.ts
@@ -8,100 +8,130 @@ const desertHotels: Hotels = [
     name: 'Sahara Oasis Villa',
     location: 'Sahra Çölü, Fas',
     image: require('../DesertImage/d1.png'),
-    rooms: [
-      { roomId: 1, type: 'Single', price: 250, bedCount: 1, bathroomCount: 1, hasBarbecue: true, hasPool: true, hasKitchen: true },
-      { roomId: 2, type: 'Double', price: 400, bedCount: 2, bathroomCount: 1, hasBarbecue: true, hasPool: true, hasKitchen: true }
-    ]
+    coordinate: {
+      latitude: 0,
+      longitude: 0
+    },
+    price: 350,
+    star: '4,6',
+    images: undefined
   },
   {
     id: 2,
     name: 'Marrakech Desert Lodge',
     location: 'Marrakeş, Fas',
     image: require('../DesertImage/d2.png'),
-    rooms: [
-      { roomId: 1, type: 'Single', price: 270, bedCount: 1, bathroomCount: 1, hasBarbecue: true, hasPool: false, hasKitchen: true },
-      { roomId: 2, type: 'Suite', price: 450, bedCount: 2, bathroomCount: 1, hasBarbecue: true, hasPool: false, hasKitchen: true }
-    ]
+    coordinate: {
+      latitude: 0,
+      longitude: 0
+    },
+    price: 450,
+    star: '4,8',
+    images: undefined
   },
   {
     id: 3,
     name: 'Desert Mirage Retreat',
     location: 'Wadi Rum, Ürdün',
     image: require('../DesertImage/d3.png'),
-    rooms: [
-      { roomId: 1, type: 'Single', price: 220, bedCount: 1, bathroomCount: 1, hasBarbecue: false, hasPool: false, hasKitchen: true },
-      { roomId: 2, type: 'Double', price: 340, bedCount: 2, bathroomCount: 1, hasBarbecue: false, hasPool: false, hasKitchen: true }
-    ]
+    coordinate: {
+      latitude: 0,
+      longitude: 0
+    },
+    price: 300,
+    star: '4,2',
+    images: undefined
   },
   {
     id: 4,
     name: 'Dubai Desert Haven',
     location: 'Dubai, Birleşik Arap Emirlikleri',
     image: require('../DesertImage/d4.png'),
-    rooms: [
-      { roomId: 1, type: 'Single', price: 300, bedCount: 1, bathroomCount: 1, hasBarbecue: true, hasPool: true, hasKitchen: true },
-      { roomId: 2, type: 'Suite', price: 500, bedCount: 2, bathroomCount: 2, hasBarbecue: true, hasPool: true, hasKitchen: true }
-    ]
+    coordinate: {
+      latitude: 0,
+      longitude: 0
+    },
+    price: 600,
+    star: '3,1',
+    images: undefined
   },
   {
     id: 5,
     name: 'Oasis Desert Retreat',
     location: 'Nevşehir, Türkiye',
     image: require('../DesertImage/d5.png'),
-    rooms: [
-      { roomId: 1, type: 'Single', price: 230, bedCount: 1, bathroomCount: 1, hasBarbecue: true, hasPool: false, hasKitchen: true },
-      { roomId: 2, type: 'Double', price: 370, bedCount: 2, bathroomCount: 1, hasBarbecue: true, hasPool: false, hasKitchen: true }
-    ]
+    coordinate: {
+      latitude: 0,
+      longitude: 0
+    },
+    price: 500,
+    star: '3,4',
+    images: undefined
   },
   {
     id: 6,
     name: 'Sand Dunes Lodge',
     location: 'Dakhla, Fas',
     image: require('../DesertImage/d6.png'),
-    rooms: [
-      { roomId: 1, type: 'Single', price: 240, bedCount: 1, bathroomCount: 1, hasBarbecue: true, hasPool: false, hasKitchen: true },
-      { roomId: 2, type: 'Double', price: 380, bedCount: 2, bathroomCount: 1, hasBarbecue: true, hasPool: false, hasKitchen: true }
-    ]
+    coordinate: {
+      latitude: 0,
+      longitude: 0
+    },
+    price: 500,
+    star: '3,7',
+    images: undefined
   },
   {
     id: 7,
     name: 'Saharan Dream House',
     location: 'Sahra Çölü, Cezayir',
     image: require('../DesertImage/d7.png'),
-    rooms: [
-      { roomId: 1, type: 'Single', price: 260, bedCount: 1, bathroomCount: 1, hasBarbecue: true, hasPool: true, hasKitchen: true },
-      { roomId: 2, type: 'Suite', price: 430, bedCount: 2, bathroomCount: 2, hasBarbecue: true, hasPool: true, hasKitchen: true }
-    ]
+    coordinate: {
+      latitude: 0,
+      longitude: 0
+    },
+    price: 600,
+    star: '3,9',
+    images: undefined
   },
   {
     id: 8,
     name: 'Red Sands Lodge',
     location: 'Riyad, Suudi Arabistan',
     image: require('../DesertImage/d8.png'),
-    rooms: [
-      { roomId: 1, type: 'Single', price: 250, bedCount: 1, bathroomCount: 1, hasBarbecue: false, hasPool: false, hasKitchen: true },
-      { roomId: 2, type: 'Double', price: 400, bedCount: 2, bathroomCount: 1, hasBarbecue: false, hasPool: false, hasKitchen: true }
-    ]
+    coordinate: {
+      latitude: 0,
+      longitude: 0
+    },
+    price: 200,
+    star: '4,2',
+    images: undefined
   },
   {
     id: 9,
     name: 'Sandstorm Retreat',
     location: 'Lalibela, Etiyopya',
     image: require('../DesertImage/d9.png'),
-    rooms: [
-      { roomId: 1, type: 'Single', price: 210, bedCount: 1, bathroomCount: 1, hasBarbecue: true, hasPool: false, hasKitchen: true },
-      { roomId: 2, type: 'Double', price: 340, bedCount: 2, bathroomCount: 1, hasBarbecue: true, hasPool: false, hasKitchen: true }
-    ]
+    coordinate: {
+      latitude: 0,
+      longitude: 0
+    },
+    price: 500,
+    star: '3,1',
+    images: undefined
   },
   {
     id: 10,
     name: 'Desert Sunset Lodge',
     location: 'Tunis, Tunus',
     image: require('../DesertImage/d10.png'),
-    rooms: [
-      { roomId: 1, type: 'Single', price: 220, bedCount: 1, bathroomCount: 1, hasBarbecue: true, hasPool: false, hasKitchen: true },
-      { roomId: 2, type: 'Suite', price: 370, bedCount: 2, bathroomCount: 2, hasBarbecue: true, hasPool: false, hasKitchen: true }
-    ]
+    coordinate: {
+      latitude: 0,
+      longitude: 0
+    },
+    price: 400,
+    star: '4,3',
+    images: undefined
   }
 ];
 
